00c29847c2f2df39eaf9be70b76292fca67dc177
bkpasa <i65593@Balkrishnas-MacBook.local>
2016-02-25 17:12:27 +0545

    logger function implemented
    l

--- addon/components/sentiment-widget.js
+++ addon/components/sentiment-widget.js
@@ -3,14 +3,18 @@ import layout from '../templates/components/sentiment-widget';
 
 export default Ember.Component.extend({
   layout: layout,
+  question: ''
+  kibanaUrl: '',
+  userInfo: '',
 
   comment: 'Tell us more...',
   showQuestion: false,
   yesClass:'',
   noClass:'',
-  showNotes: function(){
+
+  showNotes: Ember.computed('choice', function(){
     return (this.get('choice')!==null);
-  }.property('choice'),
+  }),
   choice: null,
   /*mouseEnter: function(event){
     var element = Ember.$(event.target).context;
@@ -56,6 +60,51 @@ export default Ember.Component.extend({
     this.set('noClass', '');
     this.set('choice', null);
   },
+  userName: function() {
+        var userInfo = this.get('userInfo');
+        var nameElements = [];
+
+        if(userInfo.firstName) {
+            nameElements.push(userInfo.firstName);
+        }
+        if(userInfo.middleInitial) {
+            nameElements.push(userInfo.middleInitial);
+        }
+        if(userInfo.lastName) {
+            nameElements.push(userInfo.lastName);
+        }
+        var fullName = nameElements.join(' ');
+        return fullName;
+    },
+    userId: function() {
+        var userInfo = this.get('userInfo');
+        return userInfo.userId;
+    },
+    customerCode: function(){
+        var userInfo = this.get('userInfo');
+        return userInfo.customerCode;
+    },
+    officeCode: function(){
+        var userInfo = this.get('userInfo');
+        return userInfo.officeCode;
+    },
+  //hashObject is nameValue pair, key is name and attribute value is the value
+  logUserSentimentsQuestion: function(hashObject){
+    var loggerUrl = this.get('kibanaUrl') + "?userName=" + this.userName() + "&userId=" + this.userId() + "&customerCode=" + this.customerCode() + "&officeCode=" + this.officeCode() + "&companyCode="+this.customerCode()+this.officeCode();
+        for (var key in hashObject) {
+            if (hashObject.hasOwnProperty(key)) {
+                loggerUrl += '&' + key + '=' + hashObject[key];
+            }
+        }
+        loggerUrl += '&eol=crlf';
+        Ember.$.get(loggerUrl)
+            .done(function() {
+                //
+            })
+            .fail(function() {
+                //
+            });
+  },
   actions:{
     cancelFeedback: function(){
       //kibana log
@@ -70,9 +119,11 @@ export default Ember.Component.extend({
     },
     submit: function(){
       //kibana log
+      var _this = this;
       var action = this.get('choice')? 'Like':'Dislike';
       var comment = this.get('comment')==='Tell us more...'?'':this.get('comment');
-      this.container.lookup('global:main').logUserSentimentsQuestion({
+      this.logUserSentimentsQuestion({,
+                'question': _this.get('question'),
                 'action': action,
                 'comment': comment
             });
